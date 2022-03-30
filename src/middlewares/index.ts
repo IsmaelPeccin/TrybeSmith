d18ef37fbeb52d37947108a12bc75c8507e666e6
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 15:34:40 -0300

    refactor: validações sem usar a biblioteca Joi

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,4 @@
 export { default as errorMiddleware } from './error.middleware';
 export { default as jwtAuth } from './jwtAuth';
+export { default as nameValidate } from './nameValidate';
+export { default as amountValidate } from './amountValidate';
