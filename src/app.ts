7541c8275fd320c1ab0efe41e21b5c86923d0669
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 19:00:42 -0300

    refact: inclui userRouter

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
 import { errorMiddleware } from './middlewares';
-import productRouter from './routes/productsRoutes';
+import { productRouter, userRouter } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/products', productRouter);
+
+app.use('/users', userRouter);
 
 app.use(errorMiddleware);
 
