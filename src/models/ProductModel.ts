8a73dea59dce99de737f08a679ae65fd2a27779d
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 14:03:49 -0300

    feat: implementa função create no model

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,5 +1,5 @@
-import { Pool } from 'mysql2/promise';
-import { IListAllProducts } from '../interfaces';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { IListAllProducts, IAddProduct } from '../interfaces';
 import connection from './connection';
 
 export default class ProductModel {
@@ -17,5 +17,14 @@ export default class ProductModel {
     const [rows] = result;
 
     return rows as IListAllProducts[];
+  };
+
+  create = async (product: IAddProduct): Promise<IAddProduct> => {
+    const queryCreate = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
+    const { name, amount } = product;
+    const result = await this.connection.execute<ResultSetHeader>(queryCreate, [name, amount]);
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...product };
   };
 }
