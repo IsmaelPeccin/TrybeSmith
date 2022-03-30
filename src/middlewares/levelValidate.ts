b81c1bb025ad13ed2dec13ce4a1c3ef389a5b73b
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 15:55:35 -0300

    feat: validação para level

---
+++ src/middlewares/levelValidate.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateLevel = (req: Request, res: Response, next: NextFunction) => {
+  const { level } = req.body;
+
+  if (!level) {
+    return res.status(400).json({ error: 'Level is required' });
+  }
+  if (typeof level !== 'number') {
+    return res.status(422).json({ error: 'Level must be a number' });
+  }
+  if (level < 0) {
+    return res.status(422).json({ error: 'Level must be greater than 0' });
+  }
+  return next();
+};
+
+export default validateLevel;
