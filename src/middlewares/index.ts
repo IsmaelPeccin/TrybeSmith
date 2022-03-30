4c84e1ef6e9e1a3278328edc294acd70adb83682
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 15:57:07 -0300

    refactor: inclui novos arquivos no index.ts

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,7 @@ export { default as errorMiddleware } from './error.middleware';
 export { default as jwtAuth } from './jwtAuth';
 export { default as nameValidate } from './nameValidate';
 export { default as amountValidate } from './amountValidate';
+export { default as usernameValidate } from './usernameValidate';
+export { default as levelValidate } from './levelValidate';
+export { default as classeValidate } from './classeValidate';
+export { default as passwordValidate } from './passwordValidate';
