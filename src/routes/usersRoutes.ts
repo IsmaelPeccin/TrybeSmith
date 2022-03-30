84e8c82e291f81502ee5ac1bfcee689ba9550414
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 19:03:44 -0300

    feat: implementa rota users

--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,7 +1,20 @@
-import { Router } from 'express';
+import { Request, Response, Router } from 'express';
+import UserController from '../controller/UserController';
+import { usernameValidate, classeValidate, levelValidate, passwordValidate } from '../middlewares';
+
+const userController = new UserController();
 
 const userRouter = Router();
 
-userRouter.get('/');
+userRouter.post(
+  '/',
+  usernameValidate,
+  classeValidate,
+  levelValidate,
+  passwordValidate,
+  async (req: Request, res: Response) => {
+    await userController.create(req, res);
+  },
+);
 
 export default userRouter; 
