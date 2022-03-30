941315339059f64c8052e1ac72c57865fcf8f06d
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-29 21:57:39 -0300

    Reafatora o app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
 import { errorMiddleware } from './middlewares';
+import productRouter from './routes/productsRoutes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productRouter);
 
 app.use(errorMiddleware);
 
