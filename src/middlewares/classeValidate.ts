5abe3eeaaa3b8eccca80203b3a665353cea9f705
Ismael Peccin <ismaelpeccin@gmail.com>
2022-03-30 15:55:16 -0300

    feat: validação para classe

---
+++ src/middlewares/classeValidate.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateClasse = (req: Request, res: Response, next: NextFunction) => {
+  const { classe } = req.body;
+
+  if (!classe) {
+    return res.status(400).json({ error: 'Classe is required' });
+  }
+  if (typeof classe !== 'string') {
+    return res.status(422).json({ error: 'Classe must be a string' });
+  }
+  if (classe.length < 2) {
+    return res.status(422).json({ error: 'Classe must be longer than 2 characters' });
+  }
+  return next();
+};
+
+export default validateClasse;
